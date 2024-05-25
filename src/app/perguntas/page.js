a15554a88f3b3905e8639aed292fb265827209eb
Jeniffer <122577117+JenifferFerraz@users.noreply.github.com>
2024-05-25 19:30:37 -0300

    ver se vão querer animação

--- src/app/perguntas/page.js
+++ src/app/perguntas/page.js
@@ -3,10 +3,13 @@ import Link from 'next/link'
 export default function Perguntas() {
   return (
     <div className="flex flex-col h-screen bg-purple-bg p-10">
-      {/* Parte superior da página */}
       <div className="flex justify-start items-start h-2/4">
         <div className="relative w-64 md:w-80 h-64 md:h-80">
-          <img src="/assets/Group 1.svg" alt="" className="w-full h-full" />
+          <img
+            src="/assets/Group 1.svg"
+            alt=""
+            className="w-full h-full animate-bounce"
+          />
           <div className="absolute top-0 left-full w-[300%] h-32 bg-gray-base rounded-2xl p-4 shadow-lg mt-4 ml-4">
             <p className="text-purple-dark text-lg font-montserrat-alternates	text-medium">
               Olá, eu sou Atena! Para te ajudar a escolher o banco de dados que
@@ -18,10 +21,8 @@ export default function Perguntas() {
         </div>
       </div>
 
-      {/* Parte inferior da página */}
       <div className="flex justify-center items-center h-2/4">
         <div>
-          {/* Centralize o botão na tela */}
           <Link href="/respostas">
             <button className="px-6 py-3 bg-gray-base text-purple-dark rounded-2xl shadow-md w-80 h-16 hover:bg-purple-light  hover:text-white transition duration-300 ease-in-out font-montserrat-alternates hover:border-white border">
               CONTINUAR{' '}
